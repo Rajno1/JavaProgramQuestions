cb96dfe31c2c520a9ca7f84862a76a8633617d31
Rajasekhar <50586364+Rajno1@users.noreply.github.com>
2024-06-06 12:50:17 +0530

    Update FibonacciBasedonUserInput.java

--- src/JavaPrograms/FibonacciBasedonUserInput.java
+++ src/JavaPrograms/FibonacciBasedonUserInput.java
@@ -5,20 +5,34 @@ import java.util.Scanner;
 public class FibonacciBasedonUserInput {
 
 	public static void main(String[] args) {
-		int a=0,b=1,c;
-		
-	    System.out.println("How many numbers you want in Fibonacci:");
+
+		        // Fibonacci is a number series in which each number is the result of the sum of the previous two numbers
+        // 0 1 1 2 3 5 8 13 21 34 55 ----
+
+      //  int count = 10;
+
+        int firstNum = 0;
+        int secondNum = 1;
+
+        System.out.print("Enter count value: ");
+
+
         Scanner scanner = new Scanner(System.in);
         int count = scanner.nextInt();
-        System.out.print("Fibonacci Series of "+count+" numbers:");
+
+        System.out.print(firstNum + " ");
+
+        for(int i=0; i<count; i++){
+
+            System.out.print(secondNum + " ");
+
+            int temp = firstNum;
+            firstNum = secondNum;
+            secondNum = temp + firstNum;
+        }
+
 		
-		for(int i=1;i<=count;i++) {
-			c=a+b;
-			System.out.print(a+" ");
-			a=b;
-			b=c;
-			
-		}
+
 	}
 
 }
